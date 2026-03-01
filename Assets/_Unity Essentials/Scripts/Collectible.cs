715658863cca29fcb269bae5bcfd396643eec9d0
nellsnem <herasymenko.snizhana1123@vu.cdu.edu.ua>
2026-03-01 16:25:23 +0200

    Collect the collectible

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed;
+    public GameObject onCollectEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,5 +14,16 @@ public class Collectible : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy the collectible
+            Destroy(gameObject);
+            // instantiate the particle effect
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
+        
     }
 }
